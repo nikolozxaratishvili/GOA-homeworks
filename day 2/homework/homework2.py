8f8269199ed7e433aa5d3eb28d5e27195522081b
nikolozxaratishvili <nikolozixaratishvili6@gmail.com>
2026-02-06 19:19:31 +0400

    Update homework2.py

---
+++ day 2/homework/homework2.py
@@ -0,0 +1,26 @@
+birth_year = 2011
+current_year = 2025
+
+age = current_year - birth_year
+
+print(age)
+
+number1 = 10
+number2 = 5
+
+print("ჯამი:", number1 + number2)
+print("სხვაობა:", number1 - number2)
+print("ნამრავლი:", number1 * number2)
+print("გაყოფა:", number1 / number2)
+print("ნაშთი:", number1 % number2)
+
+number1 = 4
+number2 = 8
+number3 = 6
+number4 = 10
+number5 = 2
+
+numbers = number1 + number2 + number3 + number4 + number5
+average = numbers / 5
+
+print(average)
