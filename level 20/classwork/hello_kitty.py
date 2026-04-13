231021bd4a8ae07540bb30429b6b6c2975b4c3f7
nikolozxaratishvili <nikolozixaratishvili6@gmail.com>
2026-04-13 22:34:49 +0400

    day 20

---
+++ level 20/classwork/hello_kitty.py
@@ -0,0 +1,13 @@
+movies = [
+    "iron man",
+    "world war z",
+    "star wars",
+    "Matrix",
+    "Hello Kitty",
+    "Avatar"
+]
+
+if movies[4] == "Hello Kitty":
+    movies[4] = "John Wick"
+
+    print(movies)
---
+++ level 20/classwork/სკოლის_ქულები.py
@@ -0,0 +1,3 @@
+grades = [95, 88, 76, 100, 89]
+
+print(grades)
